c40a08e3660a1b881705c7657f56ed1a6f42f835
Edvinas1982 <edvinas.benetis@gmail.com>
2021-03-26 15:32:41 +0200

    Second TEST submit witch changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
 public class Main {
-    public static void main(String[] args) { System.out.println("Testuojam ir tikrinam");}
+    public static void main(String[] args)
+    { System.out.println("Testuojam ir tikrinam");}
 }
